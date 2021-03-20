3637df10d5e9c4862ba98d7a126c0d624021d0ac
BitNexuz <54295481+BitNexuz@users.noreply.github.com>
2021-03-20 14:35:08 -0300

    Atualização do "MovementThru" para tarefa
    Tarefa do dia 19/03

--- Assets/Script/MovingThruPoints.cs
+++ Assets/Script/MovingThruPoints.cs
@@ -4,26 +4,66 @@ using UnityEngine;
 
 public class MovingThruPoints : MonoBehaviour
 {
-    public float speed;
-    public Transform[] point;
-    int index;
-    void Start()
+    //Declarando as variaveis
+    /*As primeiras variaveis declaradas são um array que guardara os waypoints
+    e um index que indicara para qual waypoint o jogador deve ir no momento*/
+
+    public GameObject[] waypoints;
+    int currentWayPoint = 0;
+
+    //As outras passam os atributos do jogador (velocidade, precisão e velocidade de rotação
+
+    public float speed = 3.0f; 
+    float accuracy = 3f;
+    float rotSpeed = 0.5f;
+
+    private void Start()
     {
-        index = 0;
+        //Aqui o sistema procura pelos objetos com a tag waypoint
+
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, point[index].position, step);
-        if(transform.position == point[index].position)
+        /*nesse primeiro if, conferimos se realmente há waypoints no array, caso não tenha ele retorna,
+        impedindo que o codigo seja executado sem que haja para onde ir*/
+
+        if (waypoints.Length == 0) return;
+
+        /*Aqui a movimentação do cubo é definida, e ele é programado para se mover até os waypoints
+        e para ir "olhando" para ele utilizando o rotation*/
+
+        Vector3 lookAtGoal = new Vector3(waypoints[currentWayPoint].transform.position.x,
+        this.transform.position.y, waypoints[currentWayPoint].transform.position.z);
+
+        Vector3 direction = lookAtGoal - this.transform.position;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+             Quaternion.LookRotation(direction),
+        Time.deltaTime * rotSpeed);
+
+        /*Nesse if é ordenado que o caso o jogador esteja na posição de um dos waypoints, o waypoint atual
+        seja atualizado e assim o cubo parta para o proximo waypoint da lista*/
+
+        if (direction.magnitude < accuracy)
         {
-            index++;
-            if(index == point.Length)
+            //incrementando waypoint atual 
+
+            currentWayPoint++;
+
+            //Caso não exista um proximo waypoint, o cubo devera retornar até o primeiro waypoint
+
+            if(currentWayPoint >= waypoints.Length)
             {
-                index = 0;
+                currentWayPoint = 0;
             }
+
+            
         }
+        // atualiza a posição do cubo
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+
     }
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-# IAVector3
